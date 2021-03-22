a4ee2c2d59f3523317603155b0b8125323eb6476
Oleksii Grygorchuk <o.grygorchuk@snapaddy.com>
2021-03-22 22:52:06 +0100

    add webpack plugins, update gitignore, add src folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.expots = {
-    context: path.resolve(__dirname, 'src');
-    entry: {
-        main: './index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    }
-}
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: "./index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  resolve: {
+      extensions: ['.js'],
+      alias: {
+          '@': path.resolve(__dirname, "src")
+      }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+  ],
+};
