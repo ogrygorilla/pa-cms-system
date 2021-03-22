a6c19e094ba168235b23401717c63e8c7947895f
Oleksii Grygorchuk <o.grygorchuk@snapaddy.com>
2021-03-22 23:43:11 +0100

    add webpack loaders(for css), add babel + runtime regenerator(for compability with diverse browser options)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import "core-js/stable";
+import "regenerator-runtime";
+
+import "./scss/index.scss";
+import "./module";
+
+
 console.log('start');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -12,10 +13,10 @@ module.exports = {
     filename: "[name].bundle.js",
   },
   resolve: {
-      extensions: ['.js'],
-      alias: {
-          '@': path.resolve(__dirname, "src")
-      }
+    extensions: [".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -29,5 +30,26 @@ module.exports = {
         },
       ],
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].bundle.css",
+    }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
